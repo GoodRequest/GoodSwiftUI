b1e23f43145486abb6db30bb9b2e67b0b15674c2
Filip Sasala <31418257+plajdo@users.noreply.github.com>
2024-09-16 14:44:45 +0200

    task: Dependencies update

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/goodrequest/goodextensions-ios", branch: "feature/swift6")
+        .package(url: "https://github.com/goodrequest/goodextensions-ios", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
         .target(
